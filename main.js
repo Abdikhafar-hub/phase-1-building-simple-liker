fbe6ae60a9df92015b1f707cd12bc675f5813eea
Abdikhafar <abdikhafarissack@gmail.com>
2024-07-22 14:50:35 +0300

    first commit

--- main.js
+++ main.js
@@ -3,6 +3,52 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// Provided mimicServerCall function
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      let isRandomFailure = Math.random() < .2
+      if (isRandomFailure) {
+        reject("Random server error. Try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
+    }, 300);
+  });
+}
+
+// Add event listeners to heart icons
+document.addEventListener("DOMContentLoaded", () => {
+  const hearts = document.querySelectorAll(".like-glyph");
+  hearts.forEach(heart => {
+    heart.addEventListener("click", () => {
+      handleHeartClick(heart);
+    });
+  });
+});
+
+function handleHeartClick(heart) {
+  mimicServerCall()
+    .then(() => {
+      if (heart.classList.contains("activated-heart")) {
+        heart.classList.remove("activated-heart");
+        heart.innerText = "♡";
+      } else {
+        heart.classList.add("activated-heart");
+        heart.innerText = "♥";
+      }
+    })
+    .catch((error) => {
+      const modal = document.getElementById("modal");
+      const modalMessage = document.getElementById("modal-message");
+      modalMessage.innerText = error;
+      modal.classList.remove("hidden");
+      setTimeout(() => {
+        modal.classList.add("hidden");
+      }, 3000);
+    });
+}
+
 
 
 
